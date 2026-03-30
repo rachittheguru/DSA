74b1226d5a05777b408e13565891e46c40b26727
rachittheguru <rachitsharmakkr@gmail.com>
2026-03-30 13:22:27 +0530

    leetcode problem no 965 solution

---
+++ DSA-/lc965.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+        return helper(root, root.val);
+    }
+
+    public boolean helper(TreeNode root, int val) {
+        if (root == null) return true;
+
+        if (root.val != val) return false;
+
+        return helper(root.left, val) &&
+               helper(root.right, val);
+    }
+}
