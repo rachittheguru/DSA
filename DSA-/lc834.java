b361527f4a69ec72ce60daebaeb2a57d1fed0525
rachittheguru <rachitsharmakkr@gmail.com>
2026-04-12 12:04:45 +0530

    lc 834 solution

---
+++ DSA-/lc834.java
@@ -0,0 +1,46 @@
+class Solution {
+    int[] res;
+    int[] count;
+    List<Set<Integer>> tree;
+
+    public int[] sumOfDistancesInTree(int n, int[][] edges) {
+        res = new int[n];
+        count = new int[n];
+        tree = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            tree.add(new HashSet<>());
+        }
+
+        for (int[] edge : edges) {
+            tree.get(edge[0]).add(edge[1]);
+            tree.get(edge[1]).add(edge[0]);
+        }
+
+        dfs(0, -1);
+        dfs2(0, -1);
+
+        return res;
+    }
+
+    private void dfs(int node, int parent) {
+        count[node] = 1;
+
+        for (int child : tree.get(node)) {
+            if (child == parent) continue;
+
+            dfs(child, node);
+            count[node] += count[child];
+            res[node] += res[child] + count[child];
+        }
+    }
+
+    private void dfs2(int node, int parent) {
+        for (int child : tree.get(node)) {
+            if (child == parent) continue;
+
+            res[child] = res[node] - count[child] + (count.length - count[child]);
+            dfs2(child, node);
+        }
+    }
+}
