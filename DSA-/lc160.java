a8b58c35c37b35b7ec33e561eb31bf768d16967d
rachittheguru <rachitsharmakkr@gmail.com>
2026-04-22 19:40:35 +0530

    lc 160 solution

---
+++ DSA-/lc160.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB){
+        if(haedA==null  || headB ==null ) return null;
+        liStNode a=headA;
+        listNode b = headB;
+
+        while(a!=b){
+            if(a==null){
+                a=headB;
+            }else{
+                a=a.next;
+            }
+            if(b==null){
+                b=headA;
+            }
+            else{
+                b=b.next;
+            }
+        } 
+        return a;
+
+    }
+}
