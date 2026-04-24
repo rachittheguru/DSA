52daaa556fa3bb47ca265210986e88e4477bab22
rachittheguru <rachitsharmakkr@gmail.com>
2026-04-24 06:26:46 +0530

    leetcode 739 sol

--- DSA-/lc102.java
+++ DSA-/lc102.java
@@ -1,25 +1,30 @@
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (root == null) return res;
 
-        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>>res=new ArrayList<>();
+        if(root==null)return res;
+
+        Queue<Integer> q= new LinkedList<>();
         q.offer(root);
+        while(!q.isEmpty()){
+            int size=q.size();
+            List<Integer>level=new ArrayList<>();
 
-        while (!q.isEmpty()) {
-            int size = q.size();
-            List<Integer> level = new ArrayList<>();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
+            for(int i =0; i<size;i++){
+                TreeNode node =q.poll();
                 level.add(node.val);
+                if(node.left != null)q.offer(node.left);
+                if(node.right !=null)q.offer(node.right);
 
-                if (node.left != null) q.offer(node.left);
-                if (node.right != null) q.offer(node.right);
-            }
-
-            res.add(level);
         }
-        return res;
+        res.add(level);
+        }return res;
+        
     }
 }
