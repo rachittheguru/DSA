a947c9d214b0c11b81a6f7a175f59f38b35ce0ba
rachittheguru <rachitsharmakkr@gmail.com>
2026-05-04 22:46:20 +0530

    sol 48.java

--- DSA-/lc48.java
+++ DSA-/lc48.java
@@ -2,7 +2,6 @@ class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
 
-        // Step 1: Transpose the matrix
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
                 int temp = matrix[i][j];
