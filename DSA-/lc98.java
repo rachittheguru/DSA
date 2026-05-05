911ad64711caa1d17f502b3a5864cb93bf2b5a17
rachittheguru <rachitsharmakkr@gmail.com>
2026-05-05 23:55:39 +0530

    update

--- DSA-/lc98.java
+++ DSA-/lc98.java
@@ -6,7 +6,9 @@ class Solution {
     private boolean valid(TreeNode root, long min, long max) {
         if (root == null) return true;
 
-        if (root.val <= min || root.val >= max) return false;
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
 
         return valid(root.left, min, root.val) &&
                valid(root.right, root.val, max);
