6d9626be3ffcf780c453c81ad09d90d994e964d6
rachittheguru <rachitsharmakkr@gmail.com>
2026-05-06 20:12:57 +0530

    solution

---
+++ DSA-/lc1861.java
@@ -0,0 +1,32 @@
+class Solution {
+    public char[][] rotateTheBox(char[][] box) {
+        int m = box.length;
+        int n = box[0].length;
+
+        for (int i = 0; i < m; i++) {
+            int empty = n - 1;
+
+            for (int j = n - 1; j >= 0; j--) {
+                if (box[i][j] == '*') {
+                    empty = j - 1; // reset after obstacle
+                } else if (box[i][j] == '#') {
+                    // move stone to "empty" position
+                    char temp = box[i][empty];
+                    box[i][empty] = '#';
+                    box[i][j] = temp == '#' ? '#' : '.';
+                    empty--;
+                }
+            }
+        }
+
+        char[][] res = new char[n][m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res[j][m - 1 - i] = box[i][j];
+            }
+        }
+
+        return res;
+    }
+}
