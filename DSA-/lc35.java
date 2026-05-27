8d3e85366a9ae685c6f1a992b84faf1337f8d104
rachittheguru <rachitsharmakkr@gmail.com>
2026-05-27 22:07:37 +0530

    sol

--- DSA-/lc35.java
+++ DSA-/lc35.java
@@ -1,7 +1,6 @@
 class Solution {
     public int searchInsert(int[] nums, int target) { int start=0;
         int end=nums.length-1;
-
         while (start<=end){
             int mid=(start+end)/2;
             if(nums[mid]==target){
@@ -13,7 +12,5 @@ class Solution {
             }
         }
         return start;
-    
-        
     }
 }
