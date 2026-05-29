2b01d3573eb57162ec8f3ec9833a3bb9e8240993
rachittheguru <rachitsharmakkr@gmail.com>
2026-05-29 14:30:47 +0530

    sol

--- DSA-/lc3300.java
+++ DSA-/lc3300.java
@@ -1,10 +1,8 @@
 class Solution {
     public int minElement(int[] nums) {
         int ans = Integer.MAX_VALUE;
-
         for (int num : nums) {
             int sum = 0;
-
             while (num > 0) {
                 sum += num % 10;
                 num /= 10;
