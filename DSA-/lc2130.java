007164f3c635cb7c5a6e2766c112b8eb880f3f60
rachittheguru <rachitsharmakkr@gmail.com>
2026-06-14 20:27:15 +0530

    sol

---
+++ DSA-/lc2130.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int pairSum(ListNode head) {
+        List<Integer> nums = new ArrayList<>();
+
+        while (head != null) {
+            nums.add(head.val);
+            head = head.next;
+        }
+
+        int ans = 0;
+        int i = 0, j = nums.size() - 1;
+
+        while (i < j) {
+            ans = Math.max(ans, nums.get(i) + nums.get(j));
+            i++;
+            j--;
+        }
+
+        return ans;
+    }
+}
