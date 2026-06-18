c5ac628a36bf35a91042ab26911f8ac33b876c69
rachittheguru <rachitsharmakkr@gmail.com>
2026-06-18 22:57:51 +0530

    sol

--- DSA-/lc1344.java
+++ DSA-/lc1344.java
@@ -1,13 +1,8 @@
 class Solution {
     public double angleClock(int hour, int minutes) {
-        // Hour hand angle
         double hourAngle = (hour % 12) * 30 + minutes * 0.5;
-
-        // Minute hand angle
         double minuteAngle = minutes * 6;
-
         double diff = Math.abs(hourAngle - minuteAngle);
-
         return Math.min(diff, 360 - diff);
     }
 }
