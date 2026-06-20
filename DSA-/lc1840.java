5ce016759235350989fadff4eb921496bef465e0
rachittheguru <rachitsharmakkr@gmail.com>
2026-06-20 23:03:52 +0530

    sol

--- DSA-/lc1840.java
+++ DSA-/lc1840.java
@@ -8,10 +8,8 @@ class Solution {
             return n - 1;
         }
 
-        // Sort restrictions by building ID inline
         Arrays.sort(restrictions, (a, b) -> Integer.compare(a[0], b[0]));
 
-        // Step 1: Left-to-Right Pass
         restrictions[0][1] = Math.min(restrictions[0][1], restrictions[0][0] - 1);
         for (int i = 1; i < m; i++) {
             int maxAllowedFromLeft = restrictions[i - 1][1] + (restrictions[i][0] - restrictions[i - 1][0]);
@@ -30,26 +28,22 @@ class Solution {
         int maxTallestBuilding = 0;
 
         maxTallestBuilding = Math.max(maxTallestBuilding, (0 + restrictions[0][1] + restrictions[0][0] - 1) / 2);
-
         for (int i = 1; i < m; i++) {
             int posA = restrictions[i - 1][0];
             int hA = restrictions[i - 1][1];
             int posB = restrictions[i][0];
             int hB = restrictions[i][1];
-
             int currentPeak = (hA + hB + (posB - posA)) / 2;
             if (currentPeak > maxTallestBuilding) {
                 maxTallestBuilding = currentPeak;
             }
         }
-
         int lastPos = restrictions[m - 1][0];
         int lastH = restrictions[m - 1][1];
         int finalEndPeak = lastH + (n - lastPos);
         if (finalEndPeak > maxTallestBuilding) {
             maxTallestBuilding = finalEndPeak;
         }
-
         return maxTallestBuilding;
     }
 }
