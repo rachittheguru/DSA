887eef3cbbfe88222d45c8e24121a0edc70471f5
rachittheguru <rachitsharmakkr@gmail.com>
2026-06-25 21:59:09 +0530

    sol

---
+++ DSA-/lc3737.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+        int ans = 0;
+        int[] dresaniel = nums;
+        for (int i = 0; i < n; i++) {
+            int targetCount = 0;
+
+            for (int j = i; j < n; j++) {
+                if (nums[j] == target) {
+                    targetCount++;
+                }
+
+                int len = j - i + 1;
+
+                if (2 * targetCount > len) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
