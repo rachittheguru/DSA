50e1caba6aec4bd04df0fe415e5cf5bf37b06a97
rachittheguru <rachitsharmakkr@gmail.com>
2026-06-28 22:46:33 +0530

    sol

--- DSA-/lc3020.java
+++ DSA-/lc3020.java
@@ -1,18 +1,16 @@
+// hello everyone i am rachit sharma today i am here to solve the problem of finding the maximum length of a sequence in an array 
 class Solution {
     public int maximumLength(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
-
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-
         int ans = 1;
 
         if (map.containsKey(1)) {
             int count = map.get(1);
             ans = (count % 2 == 0) ? count - 1 : count;
         }
-
         for (int num : map.keySet()) {
 
             if (num == 1) continue;
