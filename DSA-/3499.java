3c5e64d595029149a98948d583d05fb82f6b6f90
rachittheguru <rachitsharmakkr@gmail.com>
2026-07-21 23:03:18 +0530

    sol

--- DSA-/3499.java
+++ DSA-/3499.java
@@ -1,26 +1,19 @@
 class Solution {
     public int maxActiveSectionsAfterTrade(String s) {
-        int oneCount = 0; // Total number of ones already present in the string
-        int maxMergedzeros = 0; // Max number ofg aditional ones we could get by merging 2 0 blocks
-        int currZeroCount = 0; // Length of the zero block we are currently walking through
-        int lastZeroCount = 0; // Length of the previous 0 block we finished
-
-        // Loop over each char
+        int oneCount = 0; 
+        int maxMergedzeros = 0; 
+        int currZeroCount = 0; 
+        int lastZeroCount = 0; 
         for(char c : s.toCharArray()){
-            if(c == '0') currZeroCount++; // Increase the continuos zero count
+            if(c == '0') currZeroCount++; 
             else {
-                // If the char is not a 0 and we have a current count of zeros then we save the previous zero block length
                 if(currZeroCount != 0) lastZeroCount = currZeroCount;
-                currZeroCount = 0; // Reset the current zero block length to zero since we are counting the continuos ones in this point
-                oneCount++; // Increase the one count
+                currZeroCount = 0; 
+                oneCount++; 
             }
-            // Updates the largest merged zero blocks
             maxMergedzeros = Math.max(maxMergedzeros, currZeroCount + lastZeroCount);
         }
-        // If there are no 1 blocks then the best zero block comes from either side, so we can't merge 0 blocks 
-        // Meaning there is  no profitable trade making the existing ones the best already
         if(maxMergedzeros == currZeroCount || maxMergedzeros == lastZeroCount) return oneCount;
-        // Return the largets gain obtainable by merging two zero blocks with the ones already present
         return oneCount + maxMergedzeros;
     }
 }
