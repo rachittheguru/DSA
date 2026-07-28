f9b9712894a15b298083393162c7133f7a55d19b
rachittheguru <rachitsharmakkr@gmail.com>
2026-07-28 23:59:54 +0530

    sol

--- DSA-/lc1464.java
+++ DSA-/lc1464.java
@@ -4,7 +4,7 @@ class Solution {
 
         for (int num : nums) {
             if (num >= max1) {
-                max2 = max1;
+      //          max2 = max1;
                 max1 = num;
             } else if (num > max2) {
                 max2 = num;
