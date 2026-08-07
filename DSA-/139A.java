202ff3c5693d7045aeabbb4b04d504af4cd42cf3
rachittheguru <rachitsharmakkr@gmail.com>
2026-08-07 13:20:59 +0530

    code forces solution 139A

---
+++ DSA-/139A.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[]args){
+        int n = sc.nextInt();
+        int [] days = new int[n];
+        for(int i =0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int i =0;
+        while(true){
+            n-=days[i];
+            if(n<=0){
+                system.out.print(i+1);
+                break;
+            }
+            i=(i+1)%7;
+        }
+        sc.close();
+        
+    }
+}
