8b4e82e1ef98e3d683f43ec569ca1bb45bbf7346
rachittheguru <rachitsharmakkr@gmail.com>
2026-08-11 23:49:44 +0530

    solution

---
+++ DSA-/lc268.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int asum = n*(n+1)/2;
+        int bsum =0;
+
+        for(int num:nums){
+            bsum+=num;
+
+
+        }
+        return asum-bsum;
+        
+    }
+}
