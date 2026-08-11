654901ad204fe8e15706b11a1e7f55e19a37c504
rachittheguru <rachitsharmakkr@gmail.com>
2026-08-11 23:50:03 +0530

    solution

--- DSA-/lc268.java
+++ DSA-/lc268.java
@@ -3,11 +3,8 @@ class Solution {
         int n = nums.length;
         int asum = n*(n+1)/2;
         int bsum =0;
-
         for(int num:nums){
             bsum+=num;
-
-
         }
         return asum-bsum;
         
