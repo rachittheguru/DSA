97fa9fe0f2ad255fbe37be2be8d2c3e521c2d15d
rachittheguru <rachitsharmakkr@gmail.com>
2026-08-19 21:58:39 +0530

    solution

--- DSA-/lc153.java
+++ DSA-/lc153.java
@@ -2,12 +2,10 @@ class Solution {
     public int findMin(int[] nums) {
         int left = 0;
         int right = nums.length - 1;
-
         while (left < right) {
             int mid = left + (right - left) / 2;
 
             if (nums[mid] > nums[right]) {
-                // Minimum is to the right of mid
                 left = mid + 1;
             } else {
                 right = mid;
