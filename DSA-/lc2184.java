ea7d111403a40a21f3a4ab2ac68f58f009c0d577
rachittheguru <rachitsharmakkr@gmail.com>
2026-08-27 23:58:11 +0530

    sol

---
+++ DSA-/lc2184.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    public long minimumTime(int[] time, int totalTrips) {
+
+        long low = 1;
+
+        long minTime = Integer.MAX_VALUE;
+
+        for (int t : time) {
+            minTime = Math.min(minTime, t);
+        }
+
+        long high = minTime * (long) totalTrips;
+
+        while (low < high) {
+
+            long mid = low + (high - low) / 2;
+
+            long trips = 0;
+
+            for (int t : time) {
+
+                trips += mid / t;
+
+                if (trips >= totalTrips) {
+                    break;
+                }
+            }
+
+            if (trips >= totalTrips) {
+                high = mid;
+            } 
+            else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+}
